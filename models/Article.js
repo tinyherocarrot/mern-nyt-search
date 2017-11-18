568d1b93a09ddd4d533635452dfd76325f66a3fe
tinyherocarrot <andrw.huang@gmail.com>
2017-11-18 11:05:40 -0600

    first commit

---
+++ models/Article.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+module.exports = Article;
