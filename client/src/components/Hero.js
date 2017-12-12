7159c7beb5479b9ba33e812f2fc50b72b8a112f1
Andrew Huang <andrw.huang@gmail.com>
2017-12-12 12:00:10 -0600

    background image and styling

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,8 +1,12 @@
 import React from "react";
-
+const style = {
+  background: `url(${require('../img/reynier-carl-440180.jpg')}) no-repeat 50% 75%`,
+  backgroundSize: `100%`,
+  height: `100%`
+}
 const Hero = () => {
   return (
-    <section className="hero is-info">
+    <section className="hero is-info" style={style}>
       <div className="hero-body">
         <div className="container">
           <h1 className="title">NYT Article Finder</h1>
